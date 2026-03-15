679f96ca324073686624b4d27b317f175621fe2b
charbel <charbell.bahry@gmail.com>
2026-03-15 21:52:55 +0000

    Implement get_post() handler and custom excption handler for http and validation errors

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from jinja2 import exceptions
+from starlette.exceptions import HTTPException as StarletteHTTPExcpetion
+from starlette.status import HTTP_422_UNPROCESSABLE_CONTENT
+from starlette.templating import _TemplateResponse
+
 
 app = FastAPI()
 
@@ -11,12 +18,14 @@ app.mount("/static", StaticFiles(directory="static"), "static")
 posts: list[dict] = [
     {
         "id": 1,
+        "title": "FastApi tutorial",
         "author": "zobr",
         "content": "zpbran zabir",
         "date_posted": "April 20, 2013",
     },
     {
         "id": 2,
+        "title": "Python tutorial",
         "author": "zobran",
         "content": "zobr narr",
         "date_posted": "June 12, 2012",
@@ -32,6 +41,71 @@ def home(request: Request):
     )
 
 
+@app.get("/posts/{id}")
+def post_page(request: Request, id: int):
+    for post in posts:
+        if post.get("id") == id:
+            return templates.TemplateResponse(
+                request,
+                context={"post": post, "title": post["title"]},
+                name="post.html",
+            )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
 @app.get("/api/posts")
 def get_posts():
     return posts
+
+
+@app.get("/api/posts/{id}")
+def get_post(id: int):
+    for post in posts:
+        if post.get("id") == id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
+@app.exception_handler(StarletteHTTPExcpetion)
+def general_exception_handler(request: Request, exception: StarletteHTTPExcpetion):
+    message = (
+        exception.detail
+        if exception.detail
+        else "Invalid input. Please check your request and try again"
+    )
+
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exception.status_code, content={"detail": message}
+        )
+
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        context={
+            "message": message,
+            "title": exception.status_code,
+            "status_code": exception.status_code,
+        },
+        status_code=exception.status_code,
+    )
+
+
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exception: RequestValidationError):
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content={"detail": exception.errors()},
+        )
+
+    return templates.TemplateResponse(
+        request,
+        "error.html",
+        context={
+            "message": "Invalid input please check your request and try again.",
+            "title": status.HTTP_422_UNPROCESSABLE_CONTENT,
+            "status_code": status.HTTP_422_UNPROCESSABLE_CONTENT,
+        },
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+    )
